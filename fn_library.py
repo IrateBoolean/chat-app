796cf991bbf47b025fed5ebd50d7154f91aaa7a4
LeATeP <74774288+LeATeP@users.noreply.github.com>
2021-06-11 11:55:00 +0400

    Update fn_library.py
    just rewrited the whole thing

--- fn_library.py
+++ fn_library.py
@@ -2,11 +2,10 @@ from fn_library_Tier_2 import *
 from datetime import datetime
 
 
-async def download_history_into_page(data, room_id, sock):
+async def download_history_into_page(sock, global_data, room_id):
     try:
-        room_msg = data[room_id]
+        room_msg = global_data[room_id]
         msg_list = await get_ordered_msg_data(room_msg)
-        print(msg_list)
 
         for n in msg_list:
             await sock.send_text(n)
@@ -15,23 +14,25 @@ async def download_history_into_page(data, room_id, sock):
         pass
 
 
-async def send_msg_to_users_in_room(sock, user_list, data, user_name):
+async def send_msg_to_users_in_room(user_list, room_id, user_name, user_msg):
     try:
-        # if sender == None:
-        #     sender = sock.client[1]
-        for sock in user_list:
-            await sock.send_text(f'Message From {user_name}: {data}')
+        ref_msg = reformat_user_msg(user_name, user_msg)
+        for user in user_list[room_id]:
+            user_sock = user_list[room_id][user]
+            await user_sock.send_text(ref_msg)
+
     except Exception as ex:
         print(f'Error in send_msg_to_users_in_room - {ex}')
 
-async def disconnect_user_if_quited(sock, user_list, user_name):
+
+async def disconnect_user_if_quited(sock, user_list,room_id, user_name):
     try:
-        # if sender == None:
-        #     sender_name = sock.client[1]
-        user_list.remove(sock)
-        for sock in user_list:
+        user_list[room_id].pop(user_name)
+        for user in user_list[room_id]:
+            user_sock = user_list[room_id][user]
+
             user_quit = f'user quit {user_name}'
-            await sock.send_text(user_quit)
+            await user_sock.send_text(user_quit)
     except Exception as ex:
         print(f'Error, problem in disconnect_user_if_quited {ex}')
 
@@ -39,14 +40,18 @@ async def disconnect_user_if_quited(sock, user_list, user_name):
 async def update_history_into_DB(global_data, room_id, user_name, user_msg):
     msg_time = datetime.now().strftime("%H:%M:%S")
     try:
-        global_data[room_id].update({f'{msg_time}': {user_name: user_msg}})
+        global_data[room_id].update({msg_time: {user_name: user_msg}})
     except Exception:  # create new msg theme
         global_data[room_id] = {f'{msg_time}': {user_name: user_msg}}
-    finally:  # for degunning
-        print(global_data)
 
 
-async def reformat_user_data(data_username_roomid):
-    print('received: ', data_username_roomid)
-    data_username_roomid = data_username_roomid.split(',')
-    return data_username_roomid
+def update_user_list(sock, user_list, room_id, user_name):
+    try:
+        user_list[room_id].update({user_name: sock})
+    except Exception:
+        user_list[room_id] = {user_name: sock}
+        pass
+
+
+
+
